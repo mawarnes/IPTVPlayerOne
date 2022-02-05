b5e78a4b2efafff2a0ed4ddabcb60fd2c021b0a6
Michael Warnes <mike@warnes.com>
2022-02-05 09:35:52 +0000

    * Info.plist: * iptvplayer.Android.csproj: * iptvplayer.iOS.csproj.AssemblyReference.cache:
    * PlaylistService.cs: release 1.4

--- iptvplayer/Services/PlaylistService.cs
+++ iptvplayer/Services/PlaylistService.cs
@@ -13,17 +13,18 @@ namespace iptvplayer.Services
         public PlaylistService() { }
 
         protected override async Task InitTable()
-        { 
-            var defaultData = await db.FindWithQueryAsync<Playlist>($"select * from Playlist where Name=?", "SampleIPTV");
-            if (defaultData == null)
-            {
-                await Add(new Playlist
-                {
-                    Name = "SampleIPTV",
-                    Description = "Sample IPTV Channels",
-                    Url = @"https://raw.githubusercontent.com/mawarnes/iptv/master/channels/uk_samsung.m3u"
-                });
-            }
+        {
+            //removed as per apples comments
+            //var defaultData = await db.FindWithQueryAsync<Playlist>($"select * from Playlist where Name=?", "SampleIPTV");
+            //if (defaultData == null)
+            //{
+            //    await Add(new Playlist
+            //    {
+            //        Name = "SampleIPTV",
+            //        Description = "Sample IPTV Channels",
+            //        Url = @"https://raw.githubusercontent.com/mawarnes/iptv/master/channels/uk_samsung.m3u"
+            //    });
+            //}
         }
 
         public async Task<Playlist> Add(string url, string name, string description)
